c2fac45e8eea4f23f0ea7a7d60485cc479592e38
sheamckenziebc <sheamckenziebc@gmail.com>
2025-07-07 16:04:46 -0700

    mobile updates

--- script.js
+++ script.js
@@ -25,4 +25,28 @@ for (let i = 0; i < menuLength; i++) {
         menuItem[i].className = "active";
     }
 }
-*/ 
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Image slider logic
+    const slider = document.querySelector('.image-slider');
+    if (slider) {
+        const firstImageClone = slider.firstElementChild.cloneNode(true);
+        slider.appendChild(firstImageClone);
+    }
+});
+
+
+let lastScrollY = window.scrollY;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    if (lastScrollY < window.scrollY && window.scrollY > 100) {
+        // Scrolling down
+        header.classList.add('header-hidden');
+    } else {
+        // Scrolling up
+        header.classList.remove('header-hidden');
+    }
+    lastScrollY = window.scrollY;
+}); 
